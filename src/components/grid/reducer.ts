46a203235da73a283c841c84a39f5072e1ea25d8
gugfug <gugfug1@gmail.com>
2021-02-16 01:40:57 +1000

    split reducer

--- src/components/grid/reducer.ts
+++ src/components/grid/reducer.ts
@@ -1,5 +1,5 @@
 import { List, Map } from 'immutable';
-import { GridReducer, GridState, GridTileData } from './@types';
+import { GridAction, GridReducer, GridState, GridTileData } from './@types';
 
 enum GRID_ACTIONS {
   REVEAL_TILE = '@REVEAL_TILE',
@@ -7,52 +7,64 @@ enum GRID_ACTIONS {
   HIDE_ALL = '@HIDE_ALL',
 }
 
+const revealTile = (state: GridState, action: GridAction): GridState => {
+  const { row, column } = action.payload;
+
+  const targetTile = {
+    ...state.getIn(['tiles', row, column]),
+    hidden: false,
+  };
+
+  return state
+    .updateIn(['tiles', row, column], () => targetTile)
+    .update('prevRevealedTiles', (prev: Map<number, GridTileData>) =>
+      prev.set(targetTile.index, targetTile),
+    );
+};
+
+const removeIdentity = (state: GridState): GridState => {
+  const { result: hasIdentity } = state.get('prevRevealedTiles').reduce(
+    (prev: { result: boolean; value: string }, tile: GridTileData) => ({
+      result: prev.value === tile.color,
+      value: tile.color,
+    }),
+    { result: false, value: '' },
+  );
+
+  if (hasIdentity) {
+    return state
+      .get('prevRevealedTiles')
+      .reduce((state: GridState, revealedTile: GridTileData) => {
+        return state.updateIn(
+          ['tiles', revealedTile.row, revealedTile.column],
+          (tile: GridTileData) => ({
+            ...tile,
+            removed: true,
+          }),
+        );
+      }, state);
+  } else {
+    return state;
+  }
+};
+
+const hideAll = (state: GridState): GridState => {
+  return state
+    .update('tiles', (tiles: List<List<GridTileData>>) =>
+      tiles.map((row: List<GridTileData>) =>
+        row.map(tile => ({ ...tile, hidden: true })),
+      ),
+    )
+    .update('prevRevealedTiles', () => Map());
+};
+
 const gridReducer: GridReducer = (state, action) => {
   if (action.type === GRID_ACTIONS.REVEAL_TILE) {
-    const { row, column } = action.payload;
-
-    const targetTile = {
-      ...state.getIn(['tiles', row, column]),
-      hidden: false,
-    };
-
-    return state
-      .updateIn(['tiles', row, column], () => targetTile)
-      .update('prevRevealedTiles', (prev: Map<number, GridTileData>) =>
-        prev.set(targetTile.index, targetTile),
-      );
+    return revealTile(state, action);
   } else if (action.type === GRID_ACTIONS.REMOVE_IDENTITY) {
-    const { result: hasIdentity } = state.get('prevRevealedTiles').reduce(
-      (prev: { result: boolean; value: string }, tile: GridTileData) => ({
-        result: prev.value === tile.color,
-        value: tile.color,
-      }),
-      { result: false, value: '' },
-    );
-
-    if (hasIdentity) {
-      return state
-        .get('prevRevealedTiles')
-        .reduce((state: GridState, revealedTile: GridTileData) => {
-          return state.updateIn(
-            ['tiles', revealedTile.row, revealedTile.column],
-            (tile: GridTileData) => ({
-              ...tile,
-              removed: true,
-            }),
-          );
-        }, state);
-    } else {
-      return state;
-    }
+    return removeIdentity(state);
   } else if (action.type === GRID_ACTIONS.HIDE_ALL) {
-    return state
-      .update('tiles', (tiles: List<List<GridTileData>>) =>
-        tiles.map((row: List<GridTileData>) =>
-          row.map(tile => ({ ...tile, hidden: true })),
-        ),
-      )
-      .update('prevRevealedTiles', () => Map());
+    return hideAll(state);
   } else {
     return state;
   }
