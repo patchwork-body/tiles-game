54098232fa0f5b2884ce1f73b52ae3fc83d36ed6
gugfug <gugfug1@gmail.com>
2021-02-16 02:16:03 +1000

    snapshot for GridTile component

---
+++ src/components/grid-tile/grid-tile.spec.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import { GridTile } from './grid-tile';
+
+describe('GridTile', () => {
+  const row = 0;
+  const column = 0;
+  const removed = false;
+  const flippedTileProps: FlippedTileProps = {
+    index: 0,
+    color: 'black',
+    hidden: true,
+    clickHandler: jest.fn(),
+  };
+
+  const props: GridTileProps = {
+    row,
+    column,
+    removed,
+    flippedTileProps,
+  };
+
+  test('should match snapshot', () => {
+    const { container } = render(<GridTile {...props} />);
+    expect(container.firstElementChild).toMatchSnapshot();
+  });
+});
