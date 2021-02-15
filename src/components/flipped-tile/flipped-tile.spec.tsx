afbcea8fb83f06f269df57e5554e1454b2df0b56
gugfug <gugfug1@gmail.com>
2021-02-16 02:24:01 +1000

    FlippedTile specs

---
+++ src/components/flipped-tile/flipped-tile.spec.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import { FlippedTile } from './flipped-tile';
+
+describe('GridTile', () => {
+  const index = 0;
+  const color = 'black';
+  const hidden = true;
+  const clickHandler = jest.fn();
+
+  const props: FlippedTileProps = {
+    index,
+    color,
+    hidden,
+    clickHandler,
+  };
+
+  test('should be visible', () => {
+    const { container } = render(
+      <FlippedTile {...{ ...props, hidden: false }} />,
+    );
+    expect(container.classList.contains('tile_hidden')).toBeFalsy();
+  });
+
+  test('should match snapshot', () => {
+    const { container } = render(<FlippedTile {...props} />);
+    expect(container.firstElementChild).toMatchSnapshot();
+  });
+});
